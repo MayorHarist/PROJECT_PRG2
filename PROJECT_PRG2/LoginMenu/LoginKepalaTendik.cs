01f7d34a2243eb0c41baf641faf0996915644610
aadean11 <fiqridean@gmail.com>
2024-06-28 10:18:26 +0700

    commit

---
+++ PROJECT_PRG2/LoginMenu/LoginKepalaTendik.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PROJECT_PRG2.LoginMenu
+{
+    public partial class LoginKepalaTendik : Form
+    {
+        public LoginKepalaTendik()
+        {
+            InitializeComponent();
+        }
+    }
+}
