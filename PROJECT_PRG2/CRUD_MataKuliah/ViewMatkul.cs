82fd1500c40483bee252162f26793beec796b008
aadean11 <fiqridean@gmail.com>
2024-06-29 22:16:06 +0700

    edit matkul

--- PROJECT_PRG2/CRUD_MataKuliah/ViewMatkul.cs
+++ PROJECT_PRG2/CRUD_MataKuliah/ViewMatkul.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -20,7 +21,7 @@ namespace PROJECT_PRG2.CRUD_MataKuliah
         private void ViewMatkul_Load(object sender, EventArgs e)
         {
             // TODO: This line of code loads data into the 'fINDSMARTDataSet6.MataKuliah' table. You can move, or remove it, as needed.
-            
+            this.mataKuliahTableAdapter.Fill(this.fINDSMARTDataSet6.MataKuliah);
             // TODO: This line of code loads data into the 'fINDSMART.MataKuliah' table. You can move, or remove it, as needed.
 
 
@@ -28,7 +29,183 @@ namespace PROJECT_PRG2.CRUD_MataKuliah
 
         private void btnTampil_Click(object sender, EventArgs e)
         {
-            this.mataKuliahTableAdapter.Fill(this.fINDSMARTDataSet6.MataKuliah);
+           
+        }
+
+        private void mataKuliahBindingSource_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gdvMatkul_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnTambah_Click(object sender, EventArgs e)
+        {
+            InputMatkul inputMatkul = new InputMatkul();
+            inputMatkul.ShowDialog();
+            this.Hide();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string connectionString = "integrated security=true; data source=.; initial catalog=FINDSMART";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    SqlCommand update = new SqlCommand("sp_UpdateMatkul", connection);
+                    update.CommandType = CommandType.StoredProcedure;
+
+                    // Tambahkan parameter untuk stored procedure
+                    update.Parameters.AddWithValue("@Id_Matkul", txtIdMatkul.Text);
+                    update.Parameters.AddWithValue("@Nama", txtNama.Text);
+                    update.Parameters.AddWithValue("@Jumlah_SKS", txtSKS.Text);
+                    update.Parameters.AddWithValue("@Jenis", txtJenis.Text);
+                    update.Parameters.AddWithValue("@Semester", txtSemester.Text);
+                    update.Parameters.AddWithValue("@Status", txtStatus.Text);
+                    update.Parameters.AddWithValue("@No_Pegawai", cbPegawai.SelectedText);
+                    update.Parameters.AddWithValue("@Id_Prodi", cbProdi.SelectedText);
+
+                    // Eksekusi stored procedure
+                    update.ExecuteNonQuery();
+
+                    // Menampilkan pesan jika eksekusi berhasil
+                    MessageBox.Show("Basisdata berhasil diperbaharui", "Informasi",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    clear();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnHapus_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string connectionString = "integrated security=true; data source=DESKTOP-1B9620N\\MSSQLSERVER01; initial catalog=FINDSMART";
+                SqlConnection connection = new SqlConnection(connectionString);
+
+                connection.Open();
+
+                SqlCommand delete = new SqlCommand("sp_DeleteMatkul", connection);
+                delete.CommandType = CommandType.StoredProcedure;
+
+                delete.Parameters.AddWithValue("@Id_Matkul", txtIdMatkul.Text);
+                delete.Parameters.AddWithValue("@Status", "Tidak Aktif");
+
+                delete.ExecuteNonQuery();
+
+
+                MessageBox.Show("Data berhasil dihapus", "Informasi",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void clear()
+        {
+            txtIdMatkul.Text = "";
+            txtNama.Text = "";
+            txtSKS.Text = "";
+            txtJenis.Text = "";
+            txtSemester.Text = "";
+            txtStatus.Text = "";
+            cbPegawai.SelectedText = "";
+            cbProdi.SelectedText = "";
+        }
+
+        private void btnCari_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Validasi apakah txtIdMatkul kosong
+                if (string.IsNullOrWhiteSpace(txtIdMatkul.Text))
+                {
+                    MessageBox.Show("Silakan isi Id Mata Kuliah dahulu.", "Peringatan",
+                        MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                string connectionString = "integrated security=true; data source=DESKTOP-1B9620N\\MSSQLSERVER01; initial catalog=FINDSMART";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    DataTable dataTable = new DataTable();
+                    SqlCommand myCommand = new SqlCommand("select * from MataKuliah where Id_Matkul= @Id_Matkul", connection);
+                    myCommand.Parameters.AddWithValue("@Id_Matkul", txtIdMatkul.Text);
+                    SqlDataAdapter myAdapter = new SqlDataAdapter(myCommand);
+                    myAdapter.Fill(dataTable);
+
+                    if (dataTable.Rows.Count > 0)
+                    {
+                        txtIdMatkul.Text = dataTable.Rows[0]["Id_Matkul"].ToString();
+                        txtNama.Text = dataTable.Rows[0]["Nama"].ToString();
+                        txtSKS.Text = dataTable.Rows[0]["Jumlah_SKS"].ToString();
+                        txtJenis.Text = dataTable.Rows[0]["Jenis"].ToString();
+                        txtSemester.Text = dataTable.Rows[0]["Semester"].ToString();
+                        txtStatus.Text = dataTable.Rows[0]["Status"].ToString();
+                        cbPegawai.SelectedText = dataTable.Rows[0]["No_Pegawai"].ToString();
+                        cbProdi.SelectedText = dataTable.Rows[0]["Id_Prodi"].ToString();
+                    }
+
+                    txtNama.Enabled = true;
+                    txtSKS.Enabled = true;
+                    txtJenis.Enabled = true;
+                    txtSemester.Enabled = true;
+                    txtStatus.Enabled = true;
+                    cbPegawai.Enabled = true;
+                    cbProdi.Enabled = true;
+
+                    btnUbah.Enabled = true;
+                    btnHapus.Enabled = true;
+
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error : " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void fillByToolStripButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.dosenTableAdapter.FillBy(this.fINDSMARTDataSet1.Dosen);
+            }
+            catch (System.Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void fillByToolStripButton1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.programStudiTableAdapter.FillBy(this.fINDSMARTDataSet2.ProgramStudi);
+            }
+            catch (System.Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+
         }
     }
 }
--- PROJECT_PRG2/FINDSMARTDataSet2.Designer.cs
+++ PROJECT_PRG2/FINDSMARTDataSet2.Designer.cs
@@ -878,12 +878,17 @@ SELECT Id_Prodi, Nama, Jenjang_Pendidikan, Akreditasi, Status FROM ProgramStudi 
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "17.0.0.0")]
         private void InitCommandCollection() {
-            this._commandCollection = new global::System.Data.SqlClient.SqlCommand[1];
+            this._commandCollection = new global::System.Data.SqlClient.SqlCommand[2];
             this._commandCollection[0] = new global::System.Data.SqlClient.SqlCommand();
             this._commandCollection[0].Connection = this.Connection;
             this._commandCollection[0].CommandText = "SELECT Id_Prodi, Nama, Jenjang_Pendidikan, Akreditasi, Status FROM dbo.ProgramStu" +
                 "di";
             this._commandCollection[0].CommandType = global::System.Data.CommandType.Text;
+            this._commandCollection[1] = new global::System.Data.SqlClient.SqlCommand();
+            this._commandCollection[1].Connection = this.Connection;
+            this._commandCollection[1].CommandText = "SELECT Id_Prodi, Nama, Jenjang_Pendidikan, Akreditasi, Status FROM dbo.ProgramStu" +
+                "di";
+            this._commandCollection[1].CommandType = global::System.Data.CommandType.Text;
         }
         
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
@@ -908,6 +913,19 @@ SELECT Id_Prodi, Nama, Jenjang_Pendidikan, Akreditasi, Status FROM ProgramStudi 
             FINDSMARTDataSet2.ProgramStudiDataTable dataTable = new FINDSMARTDataSet2.ProgramStudiDataTable();
             this.Adapter.Fill(dataTable);
             return dataTable;
+        }
+        
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "17.0.0.0")]
+        [global::System.ComponentModel.Design.HelpKeywordAttribute("vs.data.TableAdapter")]
+        [global::System.ComponentModel.DataObjectMethodAttribute(global::System.ComponentModel.DataObjectMethodType.Fill, false)]
+        public virtual int FillBy(FINDSMARTDataSet2.ProgramStudiDataTable dataTable) {
+            this.Adapter.SelectCommand = this.CommandCollection[1];
+            if ((this.ClearBeforeFill == true)) {
+                dataTable.Clear();
+            }
+            int returnValue = this.Adapter.Fill(dataTable);
+            return returnValue;
         }
         
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
