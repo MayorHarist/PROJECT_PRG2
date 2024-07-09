e0426f48b6c4452bc5ae47584dd9b12942bd6638
NIDA-stack <sayamufidah@gmail.com>
2024-07-09 10:52:24 +0700

    abc abc okee

--- PROJECT_PRG2/Transaksi/trsKRPP.Designer.cs
+++ PROJECT_PRG2/Transaksi/trsKRPP.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // trsKRPP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1301, 783);
+            this.Name = "trsKRPP";
             this.Text = "trsKRPP";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
